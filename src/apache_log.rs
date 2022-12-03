db25ab05451c09b41f9a214430be08406721f3c4
Avi Chad-Friedman <avichad-friedman@avis-mbp.lan>
2022-12-03 14:10:52 -0500

    inital commit

---
+++ src/apache_log.rs
@@ -0,0 +1,83 @@
+use std::io::ErrorKind;
+use std::str::FromStr;
+use chrono::{NaiveDateTime};
+use recap::lazy_static;
+use recap::Regex;
+
+#[derive(Debug)]
+pub struct ApacheLog {
+    pub ip_address: String,
+    pub id: String,
+    pub username: String,
+    pub time: NaiveDateTime,
+    pub request: String,
+    pub resource: String,
+    pub protocol: String,
+    pub statuscode: i64,
+    pub size: i64,
+    pub referrer: String,
+    pub useragent: String,
+}
+
+impl ApacheLog {
+    pub fn ip_address(&self) -> &str {
+        &self.ip_address
+    }
+    pub fn id(&self) -> &str {
+        &self.id
+    }
+    pub fn username(&self) -> &str {
+        &self.username
+    }
+    pub fn time(&self) -> NaiveDateTime {
+        self.time
+    }
+    pub fn request(&self) -> &str {
+        &self.request
+    }
+    pub fn resource(&self) -> &str {
+        &self.resource
+    }
+    pub fn protocol(&self) -> &str {
+        &self.protocol
+    }
+    pub fn statuscode(&self) -> i64 {
+        self.statuscode
+    }
+    pub fn size(&self) -> i64 {
+        self.size
+    }
+    pub fn referrer(&self) -> &str {
+        &self.referrer
+    }
+    pub fn useragent(&self) -> &str {
+        &self.useragent
+    }
+}
+
+const APACHE_LOG_REGEX: &str = r#"^(?P<ip_address>\S+) (?P<id>\S+) (?P<username>\S+) \[(?P<time>[\w:/]+\s[+\-]\d{4})\] "(?P<request>\S+)\s?(?P<resource>\S+)?\s?(?P<protocol>\S+)?" (?P<statuscode>\d{3}|-) (?P<size>\d+|-)\s?"?(?P<referrer>[^"]*)"?\s?"?(?P<useragent>[^"]*)?"?$"#;
+
+impl FromStr for ApacheLog {
+    type Err = std::io::Error;
+
+    fn from_str(log: &str) -> Result<Self, Self::Err> {
+        lazy_static! {
+            static ref regex: Regex = Regex::new(APACHE_LOG_REGEX).unwrap();
+        }
+        let cap = regex.captures(log).ok_or(std::io::Error::new(ErrorKind::InvalidInput, format!("Log {log} unable to be parsed")))?;
+        println!("Parsing log: {}", log);
+        Ok(ApacheLog {
+            ip_address: cap.name("ip_address").unwrap().as_str().to_string(),
+            id: cap.name("ip_address").unwrap().as_str().to_string(),
+            username: cap.name("username").unwrap().as_str().to_string(),
+            time: NaiveDateTime::parse_from_str(cap.name("time").unwrap().as_str(), "%d/%b/%Y:%H:%M:%S %z").unwrap(),
+            request: cap.name("request").unwrap().as_str().to_string(),
+            resource: cap.name("resource").unwrap().as_str().to_string(),
+            protocol: cap.name("protocol").expect("Couldn't parse protocol").as_str().to_string(),
+            statuscode: cap.name("statuscode").expect("Couldn't parse status_code").as_str().parse().unwrap(),
+            size: cap.name("size").unwrap().as_str().parse().unwrap(),
+            referrer: cap.name("referrer").unwrap().as_str().to_string(),
+            useragent: cap.name("useragent").unwrap().as_str().to_string(),
+        })
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,102 @@
+mod apache_log;
+
+use rusqlite::{params, Connection, Result, NO_PARAMS};
+use std::fs::File;
+use std::io;
+use std::io::BufRead;
+use std::str::FromStr;
+use chrono::{NaiveDateTime};
+use crate::apache_log::ApacheLog;
+
+fn create_table(conn: &Connection) -> Result<()> {
+    // Use a `CREATE TABLE` statement to create the `apache_logs` table
+    conn.execute("DROP TABLE IF EXISTS \"apache_logs\"", params![],)?;
+    conn.execute(
+        "CREATE TABLE apache_logs (
+            ip_address TEXT NOT NULL,
+            id TEXT NOT NULL,
+            username TEXT NOT NULL,
+            time INTEGER NOT NULL,
+            request TEXT NOT NULL,
+            resource TEXT NOT NULL,
+            protocol TEXT NOT NULL,
+            statuscode INTEGER NOT NULL,
+            size INTEGER NOT NULL,
+            referrer TEXT NOT NULL,
+            useragent TEXT NOT NULL
+        )",
+        params![],
+    )?;
+    Ok(())
+}
+
+fn insert_log(conn: &Connection, log: &ApacheLog) -> Result<()> {
+    // Use a prepared statement to insert the log entry
+    let mut stmt = conn.prepare(
+        "INSERT INTO apache_logs (ip_address, id, username, time, request, resource, protocol, statuscode, size, referrer, useragent) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+    )?;
+
+    // Bind the values from the log struct to the prepared statement
+    stmt.execute(params![
+        &log.ip_address(), &log.id(), &log.username(), &log.time().timestamp(), &log.request(), &log.resource(), &log.protocol(),
+        &log.statuscode(), &log.size(), &log.referrer(), &log.useragent()
+    ])?;
+
+    Ok(())
+}
+
+fn select_logs(conn: &Connection) -> Result<Vec<ApacheLog>> {
+    let query = "SELECT ip_address, id, username, time, request, resource, protocol, statuscode, size, referrer, useragent FROM apache_logs";
+    let mut statement = conn.prepare(query)?;
+    let apache_log_iter = statement
+        .query_map(NO_PARAMS, |row| Ok(ApacheLog {
+            ip_address: row.get(0)?,
+            id: row.get(1)?,
+            username: row.get(2)?,
+            time: NaiveDateTime::from_timestamp(row.get(3).unwrap(), 0),
+            request: row.get(4)?,
+            resource: row.get(5)?,
+            protocol: row.get(6)?,
+            statuscode: row.get(7)?,
+            size: row.get(8)?,
+            referrer: row.get(9)?,
+            useragent: row.get(10)?,
+        }))?;
+
+    let mut apache_logs: Vec<ApacheLog> = Vec::new();
+    for log in apache_log_iter {
+        let apache_log = log?;
+        apache_logs.push(apache_log);
+    }
+    Ok(apache_logs)
+}
+
+fn main() {
+    // Open the file in read-only mode
+    let file = File::open("logs.txt").unwrap();
+    let conn = Connection::open("logs.db").unwrap();
+
+    // Create a buffered reader from the file
+    let reader = io::BufReader::new(file);
+    let mut logs :Vec<ApacheLog> = Vec::new();
+    // Iterate over the lines in the file
+    for line in reader.lines() {
+        let line = line.unwrap();
+        println!("{}", line);
+        if !line.trim().is_empty() {
+            let apache_log = ApacheLog::from_str(&line);
+            match apache_log {
+                Ok(_) => logs.push(apache_log.unwrap()),
+                Err(_) => println!("Ignoring line {}", line),
+            }
+        }
+    }
+    create_table(&conn).expect("Create table failed");
+    for log in logs {
+        insert_log(&conn, &log).unwrap();
+    }
+
+    println!("{:?}", select_logs(&conn));
+
+
+}
